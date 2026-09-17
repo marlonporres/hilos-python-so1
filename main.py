adaaefc4c8afe8f7d9c93446d259025277810bfd
Marlon Porres <marlonporres75@gmail.com>
2026-09-17 16:00:55 -0600

    Se agregó la función del hilo número 3 (primos)

--- main.py
+++ main.py
@@ -14,9 +14,42 @@ def hilo_ciclos():
     time.sleep(1)
 
 def hilo_primos(a, b):
-    pass
+
+    for numero in range(a, b + 1):
+        es_primo = True
+
+        if numero < 2:
+            es_primo = False
+        else:
+            for divisor in range(2, numero):
+                if numero % divisor == 0:
+                    es_primo = False
+                    break
+        if es_primo == True:
+            print(f"{numero} es primo.")
+
 
 def main():
+    hilo1 = threading.Thread(
+        target=hilo_archivo,
+        args = (texto,)
+    )
+    hilo2 = threading.Thread(
+            target=hilo_ciclos,
+        )
+    hilo3 = threading.Thread(
+            target = hilo_primos,
+            args = (inferior, superior)
+        )
+
     texto = input("Ingrese la cadena de texto que quiere guardar en el archivo: ")
     inferior = int(input("Ingrese el rango inferior: "))
     superior = int(input("Ingrese el rango superior: "))
+
+    hilo1.start()
+    hilo2.start()
+    hilo3.start()
+
+    hilo1.join()
+    hilo2.join()
+    hilo3.join()
